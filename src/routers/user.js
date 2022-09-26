b6a1b9e5e5f947ee334316e87bef28d8b8db4539
Erick <ericckm@outlook.com.br>
2022-09-26 16:22:05 -0300

    fix bug

--- src/routers/user.js
+++ src/routers/user.js
@@ -5,19 +5,6 @@ const auth = require("../middleware/auth");
 const axios = require("axios");
 const { initialExercisesDB } = require("../../initialSetup");
 
-// router.post("/register", async (req, res) => {
-//   const user = new User(req.body);
-
-//   try {
-//     await user.save();
-//     const token = await user.generateAuthToken();
-
-//     res.status(201).send({ user, token });
-//   } catch (e) {
-//     res.status(400).send(e);
-//   }
-// });
-
 router.post("/register", async (req, res) => {
   const user = new User(req.body);
 
